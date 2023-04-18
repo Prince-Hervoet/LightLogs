c037e705e03da646d95452aa80991f0c42686db3
jammer <1378239869@qq.com>
2023-04-18 23:33:45 +0800

    feat: #add codes

--- main/main.go
+++ main/main.go
@@ -10,9 +10,21 @@ func main() {
 	test, _ := lightdouble.NewDoubleLogger(1000, 100, 200, "D:\\myprojects\\LightDouble\\test")
 	test.Start()
 	test.SetFormat("%s [%d] [%l] %s")
-	for i := 0; i < 10000; i++ {
-		test.Info(strconv.FormatInt(int64(i), 10), "这是一条日志")
+
+	go func() {
+		for i := 0; i < 10000; i++ {
+			test.Info(strconv.FormatInt(int64(i), 10), "这是一条日志")
+		}
+	}()
+
+	go func() {
+		for i := 0; i < 10000; i++ {
+			test.Info(strconv.FormatInt(int64(i), 10), "这是一条日志")
+		}
+	}()
+
+	for {
 	}
-	time.Sleep(3000)
+	time.Sleep(5000)
 	test.Close()
 }
